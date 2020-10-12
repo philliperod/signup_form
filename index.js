e4dec1edef7ebc49039faad0821fd0774ee029ef
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-12 14:07:28 -0500

    added reset if it is not an error for inputs

--- index.js
+++ index.js
@@ -14,15 +14,28 @@ form.addEventListener('submit', (enter) => {
 
   if (first === '') {
     addError('first__name', 'First Name cannot be empty');
+  } else {
+    removeError('first__name');
   }
+
   if (last === '') {
     addError('last__name', 'Last Name cannot be empty');
+  } else {
+    removeError('last__name');
   }
+
   if (email === '') {
     addError('email', 'Email address cannot be empty');
+  } else if (!validateEmail(email)) {
+    addError('email', 'Email address is not valid');
+  } else {
+    removeError('email');
   }
+
   if (password === '') {
     addError('password', 'Password cannot be empty');
+  } else {
+    removeError('password');
   }
 });
 
@@ -30,6 +43,13 @@ function addError(field, message) {
   const error = (form[field].parentNode.querySelector('em').innerText = message);
   const errorIcon = (form[field].parentNode.querySelector('img').style.display = 'block');
   const errorBorder = (form[field].parentNode.querySelector('input').style.borderColor = 'hsl(0, 100%, 74%)');
+}
+
+function removeError(field) {
+  const nonError = form[field].parentNode.querySelector('em');
+  const nonErrorIcon = (form[field].parentNode.querySelector('img').style.display = 'none');
+  const nonErrorBorder = (form[field].parentNode.querySelector('input').style.borderColor = 'hsl(248, 32%, 85%)');
+  nonError.style.opacity = '0';
 }
 
 function validateEmail(email) {
